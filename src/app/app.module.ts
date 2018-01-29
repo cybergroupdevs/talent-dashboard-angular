01f4b5891deab75bf48fbf8a9c3a179ea1809687
Neha Singh <neha.singh@cybergindia.local>
2018-01-29 18:15:23 +0530

    Added Home Route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule , 
+    RouterModule.forRoot([ {path : 'home' , component : HomeComponent} 
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
