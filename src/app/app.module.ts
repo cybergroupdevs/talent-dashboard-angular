7ca5be86713114cce47c26c28e2a072f309476de
Zeeshan Al Abedin <zeeshan.abedin@cybergindia.local>
2018-02-07 10:54:40 +0530

    Created a mock dashboard, basic Color Scheme and Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule , 
