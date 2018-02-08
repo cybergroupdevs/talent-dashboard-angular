b023f28846d00ecbddda36b579db78eaaa09a9cd
Neha Singh <neha.singh@cybergindia.local>
2018-02-09 00:44:46 +0530

    user list improvements regarding filter and ui are left

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule , 
-    RouterModule.forRoot([ {path : 'home' , component : HomeComponent} 
-    ])
+    RouterModule.forRoot([ {
+      path : 'home' , 
+      component : HomeComponent
+    }, 
+    {
+      path : 'userlist' , 
+      component : UserListComponent
+    } 
+]), 
+HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
