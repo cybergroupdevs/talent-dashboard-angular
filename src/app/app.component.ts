73c0f5a2ab39d0eb6e2847816dc8b9c980f3bec5
Lagnesh Thakur <lagneshthakur@gmail.com>
2018-02-17 18:05:22 +0530

    removed d3 fully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Node, Link } from './d3';
-import APP_CONFIG from './app.config';
 
 @Component({
   selector: 'app-root',
@@ -9,28 +7,8 @@ import APP_CONFIG from './app.config';
 })
 export class AppComponent {
   title = 'Talent Dashboard - Angular';
-  nodes: Node[] = [];
-  links: Link[] = [];
 
   constructor() {
-    const N = APP_CONFIG.N,
-          getIndex = number => number - 1;
-
-    /** constructing the nodes array */
-    for (let i = 1; i <= N; i++) {
-      this.nodes.push(new Node(i));
-    }
-
-    for (let i = 1; i <= N; i++) {
-      for (let m = 2; i * m <= N; m++) {
-        /** increasing connections toll on connecting nodes */
-        this.nodes[getIndex(i)].linkCount++;
-        this.nodes[getIndex(i * m)].linkCount++;
-
-        /** connecting the nodes before starting the simulation */
-        this.links.push(new Link(i, i * m));
-      }
-    }
   }
 
 }
--- src/app/d3/models/index.ts
+++
@@ -1,6 +0,0 @@
-// path : d3/models/index.ts
-export * from './node';
-export * from './link';
-
-// To be implemented in the next gist
-export * from './force-directed-graph';
