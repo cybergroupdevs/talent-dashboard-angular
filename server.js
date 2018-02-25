b50c1e2f1dee5772eb94d765bf9edf7d6a2c08b7
Lagnesh Thakur <lagneshthakur@gmail.com>
2018-02-25 15:27:09 +0530

    redirecting every request to index

--- server.js
+++ server.js
@@ -4,5 +4,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+// redirect all requests to index for front-end route handling
+app.get('/*', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
